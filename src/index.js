4bb4ecffcd382be534090af77062a9f304b32d33
Chris Barnes <cjbarnes312@gmail.com>
2017-10-05 21:25:23 -0400

    added 'new' route with switch

--- src/index.js
+++ src/index.js
@@ -2,22 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-//browser interacts with history library, Route = react component provides config to react router
-import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
 
+//browser interacts with history library, Route = react component provides config to react router, 
+//Switch = looks at all routes and only renders first route that matches current url
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+//importing reducers
 import reducers from './reducers';
-import PostsIndex from'./components/posts_index';
+
+//importing components
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
-
+// with switch put most specific routes at the top of the list
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     <div>
+    <Switch>
+    	<Route path="/posts/new" component={PostsNew} />
     	<Route path="/" component={PostsIndex} />
+    </Switch>
     </div>
     </BrowserRouter>
   </Provider>
