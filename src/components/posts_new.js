0d494d36d750d678aa5aab1d6590e83638f93ae0
Chris Barnes <cjbarnes312@gmail.com>
2017-10-06 15:46:25 -0400

    console logging input values

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,21 +11,28 @@ class PostsNew extends Component {
 			 	type="text"
 			 	{...field.input}
 			 />
+			 {field.meta.error}
 			</div>
 		);
 	}
 
+	onSubmit(values) {
+		console.log(values);
+	}
+
 	render() {
-		return (
-			<form>
+		const { handleSubmit } = this.props; //handleSubmit being passed to component on behalf of redux-form
+
+		return ( 
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 			 <Field
 			 	label="Post Title"
 				name="title"
 				component={this.renderField}
 			 />
 			 <Field
-			 	label="Tags"
-			 	name="tags"
+			 	label="Categories"
+			 	name="categories"
 			 	component={this.renderField}
 			 />
 			 <Field
@@ -33,11 +40,41 @@ class PostsNew extends Component {
 			 	name="content"
 			 	component={this.renderField}
 			 />
+			 <button type="submit" className="btn btn-primary"> Submit </button>
 			</form>
 		);
 	}
 }
 
+function validate(values) { //values is an object that contains all info user has entered for each property
+	
+	//console.log(values);  -> { title; "asdf", categories: "afdad", content: "adfad"}
+	const errors = {};
+
+	//Validate the inputs from 'values'
+	if (!values.title || values.title.length < 2) {
+		errors.title = "Enter a title that is at least 2 characters!";
+	}
+
+	if (!values.categories || values.categories.lenght < 2) {
+		errors.categories = "Enter some categories that are at leat 2 characters";
+	}
+
+	if (!values.content) {
+		errors.content = "Please enter content";
+	}
+
+	//If errors is empty, the form is fine to submit
+	//If errors has any properties, redux form assumes form is invalid
+	return errors
+}
+
 export default reduxForm({
+	validate,
 	form: 'PostsNewForm'
 })(PostsNew);
+
+
+
+
+//values.title -> name="title" -> field.meta.error
