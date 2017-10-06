c2a04c214476f784c38cfe8dd8e450ffb82568a5
Chris Barnes <cjbarnes312@gmail.com>
2017-10-06 15:12:34 -0400

    added forms on new route

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	renderField(field) { //field contains event handler that makes sure <Field /> is repsonsible for this input
+		return (
+			<div className="form-group">
+			 <label> {field.label} </label>
+			 <input
+			 	className="form-control"
+			 	type="text"
+			 	{...field.input}
+			 />
+			</div>
+		);
+	}
+
 	render() {
 		return (
-			<div>
-				Posts New!
-			</div>
+			<form>
+			 <Field
+			 	label="Post Title"
+				name="title"
+				component={this.renderField}
+			 />
+			 <Field
+			 	label="Tags"
+			 	name="tags"
+			 	component={this.renderField}
+			 />
+			 <Field
+			 	label="Post Content"
+			 	name="content"
+			 	component={this.renderField}
+			 />
+			</form>
 		);
 	}
 }
 
-export default PostsNew;
+export default reduxForm({
+	form: 'PostsNewForm'
+})(PostsNew);
