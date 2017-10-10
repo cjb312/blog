4e01177e0f59055456d4d5b3345748da899d2325
Chris Barnes <cjbarnes312@gmail.com>
2017-10-10 17:47:34 -0400

    changed api route to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
 export const DELETE_POST = 'delete_post';
 
-const ROOT_URL= "http://reduxblog.herokuapp.com/api";
+const ROOT_URL= "https://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=pls"
 
 export function fetchPosts() {
